27f02ca72119a305b9f98750aaef58f2d20ec3d7
Ibodullayev_Hasan <ibodullayevh040@gmail.com>
2025-01-26 12:20:43 +0500

    blog update and delete add

--- src/controllers/blog.controller.ts
+++ src/controllers/blog.controller.ts
@@ -7,12 +7,11 @@ import { validateBlogData } from "src/validation";
 import "dotenv/config";
 
 export class BlogController {
-	private static blogRepository = appDataSource.getRepository(Blog);
 
 	// All blogs of the author
 	static async getAllBlogs(req: Request, res: Response, next: NextFunction): Promise<void> {
 		try {
-			const blogs = await BlogController.blogRepository.find({
+			const blogs = await appDataSource.getRepository(Blog).find({
 				relations: {
 					author: true
 				},
@@ -45,8 +44,8 @@ export class BlogController {
 
 			blogData.author = user
 
-			const newBlog: Blog = BlogController.blogRepository.create(blogData);
-			const savedBlog = await BlogController.blogRepository.save(newBlog);
+			const newBlog: Blog = appDataSource.getRepository(Blog).create(blogData);
+			const savedBlog = await appDataSource.getRepository(Blog).save(newBlog);
 
 			res.status(201).send({
 				message: "Blog successfully created",
@@ -62,6 +61,88 @@ export class BlogController {
 					date: savedBlog.date,
 				},
 			});
+		} catch (error: any) {
+			next(new ErrorHandler(error.message, error.status || 500));
+		}
+	}
+
+
+	// update blog
+	static async updateBlog(req: Request, res: Response, next: NextFunction): Promise<void> {
+		try {
+			const blogData: IBlog = req.body;
+			const user = req.user;
+			const { id, content } = blogData;
+
+			if (!id || !content || !content.type || !content.content) {
+				throw new ErrorHandler("Blog ID, content type, and content are required.", 400);
+			}
+
+			const blogRepository = appDataSource.getRepository(Blog);
+			const blog = await blogRepository.findOne({
+				where: { id },
+				relations: { author: true },
+			});
+
+			if (!blog) {
+				throw new ErrorHandler("Blog not found.", 404);
+			}
+
+			if (blog.author?.id !== user?.id) {
+				throw new ErrorHandler("You can only update your own blogs.", 403);
+			}
+
+			const validTypes = ["text", "image", "video"];
+			if (!validTypes.includes(content.type)) {
+				throw new ErrorHandler(
+					"Invalid content type. Only 'text', 'image' or 'video' are accepted.",
+					400
+				);
+			}
+
+			if ((content.type === "image" || content.type === "video") && !content.content.startsWith("https")) {
+				throw new ErrorHandler(
+					"If the content type is 'image' or 'video', the URL must be valid.",
+					400
+				);
+			}
+
+			blog.content = content;
+			await blogRepository.save(blog);
+
+			res.status(200).send({
+				message: "Blog updated successfully.",
+				blog: {
+					id: blog.id,
+					content: {
+						type: blog.content.type,
+						content: blog.content.content,
+					},
+					author: {
+						username: blog.author?.username,
+					},
+					date: blog.date,
+				},
+			});
+		} catch (error: any) {
+			next(new ErrorHandler(error.message, error.status || 500));
+		}
+	}
+
+
+	// delete blog
+	static async deleteBlog(req: Request, res: Response, next: NextFunction): Promise<void> {
+		try {
+			const blogData: IBlog = req.body;
+			const { id } = blogData
+			if (!id) throw new ErrorHandler("id required", 400)
+
+			const blog = await appDataSource.getRepository(Blog).findOne({ where: { id: id } })
+			if (!blog) throw new ErrorHandler("blog not found", 404)
+				
+			await appDataSource.getRepository(Blog).delete(id)
+
+			res.status(200).send({ message: "Delete successfully" })
 		} catch (error: any) {
 			next(new ErrorHandler(error.message, error.status || 500));
 		}
--- src/routes/blog.routes.ts
+++ src/routes/blog.routes.ts
@@ -6,5 +6,7 @@ const blogRouter: Router = Router()
 
 blogRouter.get("/blog", verifyUser, BlogController.getAllBlogs)
 blogRouter.post("/blog/new", verifyUser, BlogController.createBlog)
+blogRouter.patch("/blog/update", verifyUser, BlogController.updateBlog)
+blogRouter.delete("/blog/remove", verifyUser, BlogController.deleteBlog)
 
 export default blogRouter
